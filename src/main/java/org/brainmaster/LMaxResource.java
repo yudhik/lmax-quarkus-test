38418b7c232c852866dc8ca55143bfd99bc2160c
Yudhi Karunia Surtan <brainmaster716@gmail.com>
2020-07-22 17:14:12 +0700

    complete code with ringbuffer

--- src/main/java/org/brainmaster/LMaxResource.java
+++ src/main/java/org/brainmaster/LMaxResource.java
@@ -1,16 +1,17 @@
 package org.brainmaster;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.context.ManagedExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.lmax.disruptor.RingBuffer;
+import io.smallrye.mutiny.Multi;
 
 @Path("/hello")
 public class LMaxResource {
@@ -20,17 +21,17 @@ public class LMaxResource {
   @Inject
   RingBuffer<ValueEvent> ringBuffer;
 
+  @Inject
+  ManagedExecutor executor;
+
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello(@QueryParam("value") int value) {
     long startTime = System.nanoTime();
-    Integer data[] = new Integer[value];
-    for (int i = 0; i < value; i++) {
-      data[i] = i;
-    }
-    Stream<Integer> stream = Arrays.stream(data);
-    // Multi.createFrom().items(stream).onItem().invoke(counter ->
-    // publishMessage(counter.intValue())).subscribe().;
+    Multi.createBy().repeating()
+        .supplier(() -> new AtomicInteger(), counter -> counter.getAndIncrement())
+        .until(counterValue -> counterValue == value).runSubscriptionOn(executor).subscribe()
+        .with(counterValue -> publishMessage(counterValue));
     return "request published in " + (System.nanoTime() - startTime) / 1_000_000 + " ms";
   }
 
